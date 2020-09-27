bdfcba4dbd40eb781224fbbd1b913c4316b1bc7d
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-09-27 16:47:20 +0545

    3)Material ui, drawer, ListView, circle-Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -8,40 +8,55 @@ void main() {
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Awesome App"),
-        ),
-        body: Container(
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Center(
+        child: Container(
           height: 100,
-          color: Colors.yellow,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: EdgeInsets.all(9),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: EdgeInsets.all(9),
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: EdgeInsets.all(9),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                alignment: Alignment.center,
-              )
-            ],
-          ),
-        ));
+          width: 100,
+          color: Colors.teal,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text("Shirshak kandel"),
+                accountEmail: Text("shirshakkandel@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://lh5.googleusercontent.com/-QZ-4aYbqPHs/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmVtbF64EAjd6Akptx0ja_vL20ZHQ/photo.jpg"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("shirshak kandel"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Ujjwal Adhikari"),
+              subtitle: Text("Marketer"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Sudan Raut"),
+              subtitle: Text("Civil engineer"),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+    return scaffold;
   }
 }

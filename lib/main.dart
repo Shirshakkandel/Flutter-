cd620247b00f3e1fc221764424a4e4f2d1ac1e4a
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-09-27 12:51:50 +0545

    1) Chapter 1

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // WidgetsApp //MaterialApp //CupertinoApp
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Container(
+        child: Text("Hi Flutter"),
+      ),
+    );
+  }
+}

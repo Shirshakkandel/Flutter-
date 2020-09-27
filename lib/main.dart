fd970ecbb972bad8efb0f8265d63545b0f7db62c
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-09-27 15:33:06 +0545

    2)Container rowand text

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // WidgetsApp //MaterialApp //CupertinoApp
   runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(
-      primarySwatch: Colors.purple,
-    ),
-  ));
+      home: Homepage(), theme: ThemeData(primaryColor: Colors.purple)));
 }
 
-class HomePage extends StatelessWidget{
+class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(
-        child: Text("Hi Flutter"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Container(
+          height: 100,
+          color: Colors.yellow,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: EdgeInsets.all(9),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: EdgeInsets.all(9),
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: EdgeInsets.all(9),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                alignment: Alignment.center,
+              )
+            ],
+          ),
+        ));
   }
 }
